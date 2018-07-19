a9019255e1ae0eb5a978784f6cf2c78e157fb54c
jolson615 <jolson615@gmail.com>
2018-07-19 11:43:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// add event listeners to each of the buttons and console log which was pressed each time one is clicked
+
+
+
+// select the card-container div using a query selector.
+
+
+
+
+// add at least 2 more people to your users array below
+let currentuser = 0
+
+let users = [
+  {
+    name: 'Ben Siegel',
+    description: 'I love my wife, my baby, my bike, and coding my face off.',
+    imageUrl: 'http://www.nationalcrimesyndicate.com/wp-content/uploads/2018/03/Siegel-UCLA-clusc_8_1_00326236a-wide-tight-1024x777.jpg'
+  },
+]
+
+// This is a LOT, so you'll need to break it down. Here are some good options for where to start.
+// Option 1: create a function that changes the user's name, image and description on the webpage. It should take the INDEX of the array as an arguement.
+// Option 2: clear out the card-container div using the innerHTML property. Then fill it up with a the skeleton code from inside the card-container div, but with names and imageURL changed.
+// Option 3: make the next and back buttons work, but for now only worry about console logging the new data.
+
+// Once you have those bits working, start thinking about how to modify them. 
