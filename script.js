87194200b832159af18cc2d67cc9f1315b017542
jolson615 <jolson615@gmail.com>
2018-07-19 11:47:56 -0400

    adds stretches

--- script.js
+++ script.js
@@ -23,4 +23,11 @@ let users = [
 // Option 2: clear out the card-container div using the innerHTML property. Then fill it up with a the skeleton code from inside the card-container div, but with names and imageURL changed.
 // Option 3: make the next and back buttons work, but for now only worry about console logging the new data.
 
-// Once you have those bits working, start thinking about how to modify them. 
+// Once you have those bits working, start thinking about how to modify them.
+
+
+
+
+// Stretch A: Remove the existing buttons, and add a "random" button that randomizes which profile is shown.
+// Stretch B: Programmatically add a list of names to the screen, and have each one so that when you click on it, that person's profile shows up.
+// Stretch C: Instead of having buttons that control the flow, just programmatically add all the user cards to the screen. You'll want to delete the buttons, (and you'll want to add the "flex-wrap: wrap;" property to the main container in the css.)
